b283fcf2eae46dcc48ac936d3f2ad9ed4a7a6295
[01;31m[K"name"[m[K: "ThuyNT13 <[01;31m[K"email"[m[K: null>
2016-11-20 15:40:07 -0600

    add iterative fibonacci

--- src/js/fibonacci.js
+++ src/js/fibonacci.js
@@ -2,7 +2,15 @@ function fibonacciIterative(number) {
   if (number <= 1) {
     return number;
   } else {
-
+    var fib = [0,1];
+    var fibSum = 0;
+    for (var i = 0; i < number-1; i++) {
+      fibSum = ((fib[0]) + (fib[1]));
+      // console.log(i+2 + ". " + fib[0] + " + " + fib[1] + " = " + fibSum);
+      fib[0] = fib[1];
+      fib[1] = fibSum;
+    }
+    return fibSum;
   }
 }
 
