66e0bcbbf1f9630a940db147b26a3c25456efaa0
[01;31m[K"name"[m[K: "ThuyNT13 <[01;31m[K"email"[m[K: null>
2016-11-20 02:31:14 -0600

    change string extraction for recursion, change base case to not return empty string

--- src/js/reverseString.js
+++ src/js/reverseString.js
@@ -1,8 +1,6 @@
 /*
 to compare runtimes for string reversal JS functions:
  http://eddmann.com/posts/ten-ways-to-reverse-a-string-in-javascript/
-basic string reversals:
-https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb#.fer9ql1zm
 */
 
 function reverseStringBuiltin(str) {
@@ -12,7 +10,7 @@ function reverseStringBuiltin(str) {
 function reverseStringIterative(str) {
   var newString = "";
   // iterate in decrementing order, adding character at index to new array
-  for (var i=str.length - 1; i >= 0; i--) {
+  for (var i = str.length - 1; i >= 0; i--) {
     newString += str[i];
   }
   return newString;
@@ -20,12 +18,12 @@ function reverseStringIterative(str) {
 
 function reverseStringRecursive(str) {
   // base case
-  if (str === "") {
-    return "";
+  if (str.length === 1) {
+    return str;
   } else {
-    // extract substring starting at 1 index and
+    // slice string starting at 1 index and
     // concatenate character at 0 index
-    return reverseStringRecursive(str.substr(1)) + str.charAt(0);
+    return reverseStringRecursive(str.slice(1)) + str[0];
   }
 
 }
