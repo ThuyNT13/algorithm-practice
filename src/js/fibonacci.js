4d3ea9197b29a0e37c497125a6b538f1d9654f2a
[01;31m[K"name"[m[K: "ThuyNT13 <[01;31m[K"email"[m[K: null>
2016-11-27 23:06:10 -0600

    add decrementing iteration and adjust incrementing for loop to initialize at 2

--- src/js/fibonacci.js
+++ src/js/fibonacci.js
@@ -2,16 +2,30 @@ function fibonacciIterative(number) {
   if (number === 1 || number === 0) {
     return number;
   }
+
+// incrementing iterative
     var fibPrev = 0;
     var fibCurrent = 1;
     var fibNext = 0;
-    for (var i = 0; i < number-1; i++) {
+    for (var i = 2; i <= number; i++) {
       fibNext = fibPrev + fibCurrent;
-      // console.log(i+2 + ". " + fib1 + " + " + fib2 + " = " + fibSum);
+      console.log(i + ". " + fibPrev + " + " + fibCurrent + " = " + fibNext);
       fibPrev = fibCurrent;
       fibCurrent = fibNext;
     }
     return fibNext;
+
+// decrementing iterative
+//     var previous = 0,
+//       current = 1,
+//       next;
+//     for (var i = number; i > 1; i--) {
+//       next = previous + current;
+//       console.log(i + ". " + previous + " + " + current + " = " + next);
+//       previous = current;
+//       current = next;
+//     }
+//     return current;
 }
 
 function fibonacciRecursive(number) {
