c8610be7af9ed066de9d4fdbf48beb6c0e036a88
Thinh Ngo <thinh@vidlet.com>
2016-12-03 15:34:19 -0800

    Potential solution

--- src/js/stockMarket.js
+++ src/js/stockMarket.js
@@ -1,33 +1,45 @@
 /*
-Suppose we could access yesterday's stock prices as an array, where:
-The indices are the time in minutes past trade opening time, which was 9:30am local time.
-The values are the price in dollars of Apple stock at that time.
-So if the stock cost $500 at 10:30am, stock_prices_yesterday[60] = 500.
-Write an efficient function that takes stock_prices_yesterday and returns the best profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
-For example:
-  stock_prices_yesterday = [10, 7, 5, 8, 11, 9]
+ Suppose we could access yesterday's stock prices as an array, where:
+ The indices are the time in minutes past trade opening time, which was 9:30am local time.
+ The values are the price in dollars of Apple stock at that time.
+ So if the stock cost $500 at 10:30am, stock_prices_yesterday[60] = 500.
+ Write an efficient function that takes stock_prices_yesterday and returns the best profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
+ For example:
+ stock_prices_yesterday = [10, 7, 5, 8, 11, 9]
 
-get_max_profit(stock_prices_yesterday)
-  returns 6 (buying for $5 and selling for $11)
+ get_max_profit(stock_prices_yesterday)
+ returns 6 (buying for $5 and selling for $11)
 
-No "shorting"—you must buy before you sell. You may not buy and sell in the same time step (at least 1 minute must pass).
-*/
+ No "shorting"—you must buy before you sell. You may not buy and sell in the same time step (at least 1 minute must pass).
+ */
 
-var profit = 0;
-function get_max_profit(stock_prices_yesterday) {
-  console.log("value - lowestPrice = currentProfit(profit)");
-  var lowestPrice = stock_prices_yesterday[0];
-  for (var indices in stock_prices_yesterday) {
-    var value = stock_prices_yesterday[indices];
-    var currentProfit = value - lowestPrice;
-    if (value < lowestPrice) {
-      console.log(value+ " - " +lowestPrice+ " = loss " +currentProfit+ "(" +profit+ ")");
-      lowestPrice = value;
-      // profit = currentProfit;
-    } else if (value >= lowestPrice && currentProfit > profit) {
-      console.log(value+ " - " +lowestPrice+ " = gain " +currentProfit+ "(" +profit+ ")");
-      profit = currentProfit;
-    }
-  }
-  return profit;
-}
+ function get_max_profit(stock_prices_yesterday) {
+     var profit = 0;
+     var lowestLoss = -Number.MAX_SAFE_INTEGER;
+     var lowestPrice = stock_prices_yesterday[0];
+     
+     for (var indices in stock_prices_yesterday) {
+         var value = stock_prices_yesterday[indices];
+         var currentProfit = value - lowestPrice;
+
+         // Handle corner case where stock price declines all day
+         if(lowestLoss !== null && currentProfit <= 0 && currentProfit > lowestLoss && indices != 0) {
+             lowestPrice = value;
+             lowestLoss = currentProfit;
+         } else if (value < lowestPrice) {
+             console.log(value+ " - " +lowestPrice+ " = loss " +currentProfit+ "(" +profit+ ")");
+             console.log(value+ " - " +lowestPrice+ " = loss " +currentProfit+ "(" +profit+ ")");
+             lowestPrice = value;
+             // profit = currentProfit;
+         } else if (currentProfit > profit) {
+             console.log(value+ " - " +lowestPrice+ " = gain " +currentProfit+ "(" +profit+ ")");
+             profit = currentProfit;
+             
+             // If we get down here, we can make a profit, so clear out lowestLoss
+             lowestLoss = null;
+         }
+     }
+
+     return lowestLoss != null ? lowestLoss : profit;
+ }
+
--- tests/spec/stockMarketSpec.js
+++ tests/spec/stockMarketSpec.js
@@ -1,16 +1,20 @@
 describe('get_max_profit()', function() {
   describe('returns the minimal loss', function() {
-    it('returns -2 for [10, 7, 6, 5, 3, 1]', function() {
+    it('returns -2 for [12, 9, 7, 5, 3, 1]', function() {
       var array = [12, 9, 7, 5, 3, 1];
       expect(get_max_profit(array)).toEqual(-2); // returns 0
     });
-    it('returns -1 for [10, 9, 8, 7, 6, 5]', function() {
+    it('returns -1 for [10, 9, 8, 7, 6, 4]', function() {
       var array1 = [10, 9, 8, 7, 6, 4];
       expect(get_max_profit(array1)).toEqual(-1); // returns 0
     });
     it('returns 0 for [9, 8, 7, 7, 7, 6]', function() {
       var array2 = [9, 8, 7, 7, 7, 6];
       expect(get_max_profit(array2)).toEqual(0);
+    });
+    it('returns 0 for [12, 12]', function() {
+      var array = [12, 12];
+      expect(get_max_profit(array)).toEqual(0); // returns 0
     });
   });
 
@@ -21,7 +25,7 @@ describe('get_max_profit()', function() {
     });
     it('returns 10 for [10, 7, 5, 15, 11, 9]', function() {
       var array4 = [10, 7, 5, 15, 11, 9];
-      expect(get_max_profit(array4)).toEqual(10);
+     expect(get_max_profit(array4)).toEqual(10);
     });
   });
 });
