8e4c3aea73b68dfd36e29ae02aa750ab4677074e
[01;31m[K"name"[m[K: "ThuyNT13 <[01;31m[K"email"[m[K: null>
2016-12-02 19:37:11 -0600

    add tests for stockMarket, including some edge cases

---
+++ tests/spec/stockMarketSpec.js
@@ -0,0 +1,14 @@
+describe('get_max_profit()', function() {
+  inspect('returns the loss in a market transaction', function() {
+    var array = [10, 7, 6, 5, 3, 1];
+    var array1 = [10, 9, 8, 7, 6, 5];
+    expect(get_max_profit(array)).toEqual(-2); // will return only latest?
+    expect(get_max_profit(array1)).toEqual(-1);
+  });
+  inspect('returns the highest profit of a market transaction', function() {
+    var array2 = [10, 7, 5, 8, 11, 9];
+    var array3 = [10, 4, 5, 6, 7, 2, 9];
+    expect(get_max_profit(array2)).toEqual(6);
+    expect(get_max_profit(array3)).toEqual(7);
+  });
+});
