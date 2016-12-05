989df57e04d1cc26625c89573e00f97bc38d224a
[01;31m[K"name"[m[K: "ThuyNT13 <[01;31m[K"email"[m[K: null>
2016-12-05 10:58:50 -0600

    reword it description in stockMarketSpec for readibility

--- tests/spec/stockMarketSpec.js
+++ tests/spec/stockMarketSpec.js
@@ -1,25 +1,25 @@
 describe('get_max_profit()', function() {
   describe('returns the minimal loss', function() {
-    it('returns -2 for [10, 7, 6, 5, 3, 1]', function() {
-      var array = [12, 9, 7, 5, 3, 1];
+    it('-2 for [12, 9, 7, 5, 3, 0]', function() {
+      var array = [12, 9, 7, 5, 3, 0];
       expect(get_max_profit(array)).toEqual(-2); // returns 0
     });
-    it('returns -1 for [10, 9, 8, 7, 6, 5]', function() {
+    it('-1 for [10, 9, 8, 7, 6, 5]', function() {
       var array1 = [10, 9, 8, 7, 6, 4];
       expect(get_max_profit(array1)).toEqual(-1); // returns 0
     });
-    it('returns 0 for [9, 8, 7, 7, 7, 6]', function() {
+    it('0 for [9, 8, 7, 7, 7, 6]', function() {
       var array2 = [9, 8, 7, 7, 7, 6];
       expect(get_max_profit(array2)).toEqual(0);
     });
   });
 
   describe('returns the highest profit', function() {
-    it('returns 7 for [10, 4, 5, 6, 7, 2, 9]', function() {
+    it('7 for [10, 4, 5, 6, 7, 2, 9]', function() {
       var array3 = [10, 4, 5, 6, 7, 2, 9];
       expect(get_max_profit(array3)).toEqual(7);
     });
-    it('returns 10 for [10, 7, 5, 15, 11, 9]', function() {
+    it('10 for [10, 7, 5, 15, 11, 9]', function() {
       var array4 = [10, 7, 5, 15, 11, 9];
       expect(get_max_profit(array4)).toEqual(10);
     });
