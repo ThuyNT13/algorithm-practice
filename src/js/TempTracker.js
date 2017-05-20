a24897d6b37a5efd74c1a820d6466acecc0a6c61
ThuyNT13 <ngo.thuy38@gmail.com>
2017-05-19 19:16:42 -0500

    Add getMax(), getMin() and getMean(); pass tests

--- src/js/TempTracker.js
+++ src/js/TempTracker.js
@@ -1,9 +1,18 @@
 function TempTracker() {
   this.minTemp = null;
   this.maxTemp = null;
+
+  this.totalNum = 0;
+  this.totalSum = 0;
+  this.mean = null;
 }
 
+// O(1)
 TempTracker.prototype.insert = function(temp) {
+  this.totalNum++;
+  this.totalSum += temp;
+  this.mean = this.totalSum/this.totalNum;
+
   if (this.maxTemp === null || temp > this.maxTemp) {
     this.maxTemp = temp;
   };
@@ -12,16 +21,19 @@ TempTracker.prototype.insert = function(temp) {
   };
 };
 
+// O(1)
 TempTracker.prototype.getMax = function() {
-  
+  return this.maxTemp;
 };
 
+// O(1)
 TempTracker.prototype.getMin = function() {
-
+  return this.minTemp;
 };
 
+// O(1)
 TempTracker.prototype.getMean = function() {
-
+  return this.mean;
 };
 
 TempTracker.prototype.getMode = function() {
--- tests/spec/TempTrackerSpec.js
+++ tests/spec/TempTrackerSpec.js
@@ -1,34 +1,36 @@
 describe('tracks the numbers added', function() {
   var tempTrack = new TempTracker();
+  tempTrack.insert(5);
+  tempTrack.insert(6);
+  tempTrack.insert(7);
+  tempTrack.insert(3);
+  console.log(tempTrack);
+
   describe('insert()', function() {
-    tempTrack.insert(5);
-    tempTrack.insert(6);
-    tempTrack.insert(7);
-    tempTrack.insert(3);
+
     it('records a new temperature', function() {
       expect(tempTrack.minTemp).toBe(3);
       expect(tempTrack.maxTemp).toBe(7);
-      console.log(tempTrack);
     });
   });
   describe('getMax()', function() {
     it('returns the highest temp so far', function() {
-      expect(getMax()).toBe();
+      expect(tempTrack.getMax()).toBe(7);
     });
   });
   describe('getMin()', function() {
     it('returns the highest temp so far', function() {
-      expect(getMin()).toBe();
+      expect(tempTrack.getMin()).toBe(3);
     });
   });
   describe('getMean()', function() {
     it('returns the average', function() {
-      expect(getMean()).toBe();
+      expect(tempTrack.getMean()).toBe(5.25);
     });
   });
   describe('getMode()', function() {
     it('returns the number that appears the most', function() {
-      expect(getMode([1, 3, 6, 3, 1, 3])).toBe(3);
+      expect(tempTrack.getMode([1, 3, 6, 3, 1, 3])).toBe(3);
     });
   });
 })
