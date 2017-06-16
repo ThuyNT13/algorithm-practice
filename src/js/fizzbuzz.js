ffdc78987d25f7376e8df868d163967576d15734
ThuyNT13 <ngo.thuy38@gmail.com>
2017-06-16 16:38:36 -0500

    Refactor fizzbuzz to split responsibility

--- src/js/fizzbuzz.js
+++ src/js/fizzbuzz.js
@@ -1,11 +1,41 @@
 function fizzbuzz(number) {
   if (number % 15 === 0) {
-    return "fizzbuzz";
+    console.log("fizzbuzz");
   } else if (number % 5 === 0) {
-    return "buzz";
+    console.log("buzz");
   } else if (number % 3 === 0) {
-    return "fizz";
+    console.log("fizz");
   } else {
-    return number;
+    console.log(number);;
   }
 }
+
+function rightPoint(maxNum, mult1, mult2) {
+  for (var i=maxNum; i>0; i--) {
+    rightPointLogic(i, mult1, mult2);
+  };
+}
+
+function rightPointLogic(num, m1, m2) {
+  var msg = "";
+
+  if (num % m2 === 0) {
+    msg += "Right";
+  }
+  if (num % m1 === 0) {
+    msg += "Point";
+  }
+  if (msg.length === 0) {
+    msg = num.toString();
+  }
+
+  print(msg);
+}
+
+function print(msg) {
+  console.log(msg);
+}
+
+rightPoint(50, 4, 5);
+
+// console.log(fizzbuzz());
