4b35faa25234fa08fe719e563099e3d505b2a358
ThuyNT13 <ngo.thuy38@gmail.com>
2017-06-16 15:46:13 -0500

    Add main getHighestProductOf3Ints; write tests, pass all

--- src/js/Product3Ints.js
+++ src/js/Product3Ints.js
@@ -2,8 +2,14 @@
 Given an array of integers, find the highest product you can get from three of the integers.
 
 Complexity:
-time: O(n log n)
-space: O()
+time: O(n log n)  with sorting
+      O(n) unsorted
+space: O(1)
+
+Bonus
+1. What if we wanted the highest product of 4 items?
+2. What if we wanted the highest product of k items?
+3. If our highest product is really big, it could overflow ↴ . How should we protect against this?
 
 */
 
@@ -13,7 +19,40 @@ function getHighestProductOf3Ints(arr) {
     throw new Error("There needs to be at least 3 numbers.");
   }
 
+  var highest = Math.max(arr[0], arr[1]);
+  var lowest = Math.min(arr[0], arr[1]);
 
+  var highestProductOf2 = arr[0] * arr[1];
+  var lowestProductOf2 = arr[0] * arr[1];
+
+  var highestProductOf3 = arr[0] * arr[1] * arr[2];
+
+  for (var i=0; i<arr.length; i++) {
+    var current = arr[i];
+
+    highestProductOf3 = Math.max(
+      highestProductOf3, // current highest stored
+      current * highestProductOf2, // current iteration
+      current * lowestProductOf2 //  current iteration for double negatives edge case
+    );
+
+    highestProductOf2 = Math.max(
+      highestProductOf2,
+      current * highest,
+      current * lowest
+    )
+
+    lowestProductOf2 = Math.min(
+      lowestProductOf2,
+      current * highest,
+      current * lowest
+    )
+
+    highest = Math.max(highest,current);
+    lowest = Math.min(lowest,current);
+  }
+
+  return highestProductOf3;
 }
 
 function sortInts(arr) {
--- tests/spec/Product3IntsSpec.js
+++ tests/spec/Product3IntsSpec.js
@@ -1,5 +1,19 @@
 describe('return highest product of 3 integers from array', function() {
   var intArr = [1, 10, -5, 1, -100];
+  var intArr2 = [5, 3, 9, 8, 6];
+
+  describe('getHighestProductOf3Ints()', function() {
+    it('returns the product of the highest 3 integers', function() {
+      expect(getHighestProductOf3Ints(intArr2)).toEqual(432);
+      expect(getHighestProductOf3Ints(intArr)).toEqual(5000);
+    });
+  })
+
+  describe('throws an error if less than 3 in array', function() {
+    it('throws Error for [1,2]', function() {
+      expect( function() { getHighestProductOf3Ints([1,2]); }).toThrow(new Error("There needs to be at least 3 numbers."));
+    });
+  });
 
   describe('sortInts()', function() {
     it('sorts', function() {
@@ -8,10 +22,4 @@ describe('return highest product of 3 integers from array', function() {
       );
     });
   });
-
-  describe('throws an error if less than 3 in array', function() {
-    it('throws Error for [1,2]', function() {
-      expect( function() { getHighestProductOf3Ints([1,2]); }).toThrow(new Error("There needs to be at least 3 numbers."));
-    })
-  })
 })
