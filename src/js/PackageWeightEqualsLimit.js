f3e0764ee2c1533d127f052a5ad704c302512460
ThuyNT13 <ngo.thuy38@gmail.com>
2018-01-31 04:43:52 -0800

    Adjust PackageWeightEqualsLimit to measure index and not value

--- src/js/PackageWeightEqualsLimit.js
+++ src/js/PackageWeightEqualsLimit.js
@@ -12,46 +12,45 @@ Constraints:
 
 - [output] array.integer
 
-
 **/
 
 function packageWeightEqualsLimit(arr, limit) {
   let charMap = {};
 
-  // for (var i=0; i<arr.length; i++) {
-  //   let char = arr[i];
-  //   let compLimit = limit-char;
-  //   // console.log(char);
-  //   if (charMap.hasOwnProperty(compLimit)) {
-  //     console.log(arr.indexOf(char)+ ":" +i);
-  //     if (char < compLimit) {
-  //       // console.log(arr.indexOf(char)+ " : " +arr.indexOf(compLimit));
-  //       return [arr.indexOf(char), arr.indexOf(compLimit)];
-  //     } else if (char > compLimit) {
-  //       return [arr.indexOf(compLimit), arr.indexOf(char)];
-  //     } else {
-  //       return [i, arr.indexOf(char)];
-  //     }
-  //   } else {
-  //     charMap[arr[i]] = 1;
-  //   }
-  // }
-
-  // NESTED LOOP
   for (var i=0; i<arr.length; i++) {
-    for (var j=0; j<arr.length; j++) {
-      console.log("i: " +arr[i]+ ", j: " +arr[j]);
-        if (arr[i] + arr[j] === limit) {
-          if (arr[i] < arr[j]) {
-            return [i,j];
-          } else if (arr[i] > arr[j]) {
-            return [j,i];
-          } else {
-            return [i,j];
-          }
-        }
+    let char = arr[i];
+    let compLimit = limit-char;
+    // console.log(char);
+    if (charMap.hasOwnProperty(compLimit)) {
+      console.log(arr.indexOf(char)+ ":" +i);
+      if (arr.indexOf(char) > arr.indexOf(compLimit)) {
+        // console.log(arr.indexOf(char)+ " : " +arr.indexOf(compLimit));
+        return [arr.indexOf(char), arr.indexOf(compLimit)];
+      } else if (arr.indexOf(char) < arr.indexOf(compLimit)) {
+        return [arr.indexOf(compLimit), arr.indexOf(char)];
+      } else {
+        return [i, arr.indexOf(char)];
+      }
+    } else {
+      charMap[arr[i]] = 1;
     }
   }
+
+  // NESTED LOOP
+  // for (var i=0; i<arr.length; i++) {
+  //   for (var j=0; j<arr.length; j++) {
+  //     console.log("i: " +arr[i]+ ", j: " +arr[j]);
+  //       if (arr[i] + arr[j] === limit) {
+  //         if (i < j) {
+  //           return [j,i];
+  //         } else if (i > j) {
+  //           return [i,j];
+  //         } else {
+  //           return [i+1, j];
+  //         }
+  //       }
+  //   }
+  // }
   // console.log("charMap: "charMap);
 
   return [];
