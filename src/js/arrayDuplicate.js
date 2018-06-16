14e4458f5bc914da81df31ec22efa55ee74ef035
ThuyNT13 <ngo.thuy38@gmail.com>
2018-06-16 00:33:37 -0700

    Rename and add method to semi-remove duplicates in-place in array

--- src/js/arrayDuplicate.js
+++ src/js/arrayDuplicate.js
@@ -31,14 +31,29 @@ const containsDuplicate = function(nums) {
 Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
 
 Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+---
+Does not change array but modifies in place by utilizing *count* to keep track of index of unique nums, and *org* to be the index iterating over the "original" array.
+
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
 */
+
 const removeDuplicates = function(nums) {
-  for (let i=1; i<nums.length; i++) {
-    if (nums[i] == nums[i-1]) {
-      nums.splice(i,1);
-      console.log(nums.length);
+  let count = 0;
+  let org = 1;
+  let result = [];
+
+  while (org < nums.length) {
+    if (nums[count] == nums[org]) {
+      org++;
+    } else {
+      count++;
+      nums[count] = nums[org];
+      org++;
     }
   }
-  console.log(nums);
-  return nums.length;
+
+  result.push(count+1);
+  result.push(nums.slice(0,count+1));
+  return result;
 }
--- tests/spec/arrayDuplicateSpec.js
+++ tests/spec/arrayDuplicateSpec.js
@@ -21,19 +21,28 @@ describe("removeDuplicates()", function() {
   describe("removes duplicates from sorted array", function() {
     it("returns length of 2 for [1,2] from [1,1,2]", function() {
       const arr = [1,1,2];
-      expect(removeDuplicates(arr)).toEqual(2);
+      const result = [2, [1,2]];
+      expect(removeDuplicates(arr)).toEqual(result);
     });
     it("returns length of 5 for [0,1,2,3,4] from [0,0,1,1,1,1,2,3,3,4]", function() {
       const arr = [0,0,1,1,1,2,2,3,3,4];
-      expect(removeDuplicates(arr)).toEqual(5);
+      const result = [5, [0,1,2,3,4]];
+      expect(removeDuplicates(arr)).toEqual(result);
     });
     it("returns length of 1 for [1] from [1,1,1,1,1,1]", function() {
       const arr = [1,1,1,1,1,1];
-      expect(removeDuplicates(arr)).toEqual(1);
+      const result = [1, [1]];
+      expect(removeDuplicates(arr)).toEqual(result);
     })
-    it("returns length of 1 for [1] from [1,1,1,1,1,1]", function() {
+    it("returns length of 5 for [1,2,3,4,5] from [1,1,2,3,4,4,5,5]", function() {
       const arr = [1,1,2,3,4,4,5,5];
-      expect(removeDuplicates(arr)).toEqual(5);
+      const result = [5, [1,2,3,4,5]];
+      expect(removeDuplicates(arr)).toEqual(result);
+    });
+    it("returns 6 for [-10,-8,-5,0,7,9] from [-10,-10,-8,-8,-8,-5,0,7,7,9]", function() {
+      const arr = [-10,-10,-8,-8,-8,-5,0,7,7,9];
+      const result = [6, [-10, -8, -5, 0, 7, 9]]
+      expect(removeDuplicates(arr)).toEqual(result);
     })
   })
 })
