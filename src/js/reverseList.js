6cdd056c48774aee08c140ad51666e5849b86bdf
Thuy <ngo.thuy38@gmail.com>
2018-10-27 17:48:21 -0700

    add javascript class implementation
    correct error

--- src/js/reverseList.js
+++ src/js/reverseList.js
@@ -1,23 +1,34 @@
-function Node(v,n) {
-  this.val = v;
-  this.next = n;
+Class Node {
+  constructor(d) {
+    this.data = d;
+    this.next = null
+  }
 }
 
-function reverseLinkedList(ll) {
-  let current = ll;
-  let next, prev;
-
-  while(current) {
-    // current.next saved to next
-    next = current.next;
-    // set reference/pointer to prev, no longer pointing to current.next/next
-    current.next = prev;
-    // increment previous to current node
-    prev = current;
-    // increment current node to next node or null at end of list
-    current = next;
+Class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0
   }
-  // final prev saved to new ll
-  ll = prev;
-  return ll;
+  
+  reverse(linkList) {
+    let current = linkList.head;
+    let prev;
+    let next;
+    
+    while(current) {
+      // increment next, point next to current.next node
+      next = current.next;
+      // set reference/pointer to prev, no longer pointing to current.next/next
+      current.next = prev;
+      // increment prev, point to current node
+      prev = current;
+      // increment current, point to next
+      current = next;
+    }
+    // point head to prev which should be the tail, with current null
+    linkList.head = prev;
+    
+    return linkList;
+  }
 }
