4f49f8764bf0590c7e72231851d5603b80a187a8
ThuyNT13 <ngo.thuy38@gmail.com>
2019-01-18 20:57:42 -0800

    Correct wording in spec

--- tests/spec/mergeArraysSpec.js
+++ tests/spec/mergeArraysSpec.js
@@ -19,14 +19,14 @@ describe("merge 2 arrays", function() {
     })
   })
   describe("when arrays have same length", function () {
-    it("returns 1st array", function () {
+    it("returns both arrays merged", function () {
       const actual = mergeArrays([3,4,5,6,7,8], [1,8,12,14,19,21]);
       const expected = [1,3,4,5,6,7,8,8,12,14,19,21];
     expect(actual).toEqual(expected);
   })
 })
   describe("when arrays have different lengths", function () {
-    it("returns 1st array", function () {
+    it("returns both arrays merged", function () {
       const actual = mergeArrays([3,4,5,6], [1,9]);
       const expected = [1,3,4,5,6,9];
       expect(expected).toEqual(actual);
