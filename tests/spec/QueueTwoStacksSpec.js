07499bfe705433d295f891af72cfe06fbc33ae7f
ThuyNT13 <ngo.thuy38@gmail.com>
2019-01-18 22:26:14 -0800

    Add tests for QueueTwoStacks

---
+++ tests/spec/QueueTwoStacksSpec.js
@@ -0,0 +1,27 @@
+describe("Implement queue with two stacks", function() {
+  const Queue = new QueueTwoStacks(); 
+  Queue.enqueue(1);
+  Queue.enqueue(2);
+  Queue.enqueue(3);
+
+  describe("enqueue()", function() {
+    it("appends an element to tail", function() {
+      Queue.enqueue(4);
+      const expected = [1,2,3,4];
+      expect(Queue.inStack).toEqual(expected);
+    })
+    it("appends an element correctly after dequeing", function() {
+      Queue.dequeue();
+      Queue.enqueue(4);
+      const expected = [2,3,4];
+      expected(Queue.inStack).toEqual(expected);
+    })
+  })
+  describe("dequeue()", function () {
+    it("removes an element from head", function () {
+      Queue.dequeue();
+      const expected = [2,3];
+      expect(Queue.inStack).toEqual(expected);
+    })
+  })
+})
