57b9057b69028bc469683cfd3e8c592a5fb9525b
ThuyNT13 <ngo.thuy38@gmail.com>
2019-01-18 23:45:20 -0800

    Add error throwing for dequeue

--- src/js/QueueTwoStacks.js
+++ src/js/QueueTwoStacks.js
@@ -9,20 +9,20 @@ class QueueTwoStacks {
   }
 
   dequeue() {
+    if (this.inStack.length === 0) {
+      throw new Error("Can't dequeue from empty queue!");
+    }
+
     while (this.inStack.length > 0) {
       this.outStack.push(this.inStack.pop());
     }
+    
     const dequeued = this.outStack.pop();
-
-    // move stack back to instack for enqueueing
+    
     while (this.outStack.length > 0) {
       this.inStack.push(this.outStack.pop());
     }
 
     return dequeued;
-  }
-
-  returnQueue() {
-    return this.inStack;
   }
 }
