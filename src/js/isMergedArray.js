bfe2bd7f3814258c7c8f753d6c4e93d362c4057f
ThuyNT13 <ngo.thuy38@gmail.com>
2019-01-23 14:10:05 -0800

    Rename files

--- src/js/isMergedArray.js
+++ src/js/isMergedArray.js
@@ -1,20 +1,20 @@
-function isSingleRiffle(half1, half2, shuffledDeck) {
+function isMergedArray(half1, half2, mergedArray) {
   let h1Index = half1.length - 1;
   let h2Index = half2.length - 1;
-  let sdIndex = shuffledDeck.length - 1;
+  let sdIndex = mergedArray.length - 1;
 
   while (sdIndex >= 0) {
     const currentH1 = half1[h1Index];
     const currentH2 = half2[h2Index];
-    const currentSd = shuffledDeck[sdIndex];
+    const currentSd = mergedArray[sdIndex];
 
     if (currentH1 != undefined && currentH1 === currentSd) {
       // half1.pop();
-      // shuffledDeck.pop();
+      // mergedArray.pop();
       h1Index--;
     } else if (currentH2 != undefined && currentH2 === currentSd) {
       // half2.pop();
-      // shuffledDeck.pop();
+      // mergedArray.pop();
       h2Index--;
     } else {
       return false;
@@ -23,7 +23,7 @@ function isSingleRiffle(half1, half2, shuffledDeck) {
     sdIndex--;
   }
 
-  // checks for false positive when there are cards left in half stacks
+  // checks for false positive when there are elements left in half stacks
   if (h1Index < 0 && h2Index < 0 && sdIndex < 0) {
     return true;
   }
--- tests/spec/isMergedArraySpec.js
+++ tests/spec/isMergedArraySpec.js
@@ -1,50 +1,50 @@
-describe("isSingleRiffle()", function() {
+describe("isMergedArray()", function() {
   describe("when both halves are the same length", function () {
     it("can return true", function() {
-      const actual = isSingleRiffle([1, 4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6]);
+      const actual = isMergedArray([1, 4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6]);
       const expected = true;
       expect(actual).toEqual(expected);
     })
     it("can return false", function() {
-      const actual = isSingleRiffle([3,4,5],[2,1,6],[1,2,3,4,5,6]);
+      const actual = isMergedArray([3,4,5],[2,1,6],[1,2,3,4,5,6]);
       const expected = false;
       expect(actual).toEqual(expected);
     })
   })
   describe("when halves are different lengths", function () {
     it("can return true", function() {
-      const actual = isSingleRiffle([2,3,4,5],[1,6],[1,2,3,4,5,6]);
+      const actual = isMergedArray([2,3,4,5],[1,6],[1,2,3,4,5,6]);
       const expected = true;
       expect(actual).toEqual(expected);
     })
     it("can return false", function () {
-      const actual = isSingleRiffle([1, 5], [2, 3, 6], [1, 2, 6, 3, 5]);
+      const actual = isMergedArray([1, 5], [2, 3, 6], [1, 2, 6, 3, 5]);
       const expected = false;
       expect(actual).toEqual(expected);
     })
   })
   describe("when one half is empty", function() {
     it("can return true", function () {
-      const actual = isSingleRiffle([], [2, 3, 6], [2, 3, 6]);
+      const actual = isMergedArray([], [2, 3, 6], [2, 3, 6]);
       const expected = true;
       expect(actual).toEqual(expected); 
     })
     it("can return false", function() {
-      const actual = isSingleRiffle([2,3,4,5],[], [1,2,3,4,5]);
+      const actual = isMergedArray([2,3,4,5],[], [1,2,3,4,5]);
       const expected = false;
       expect(actual).toEqual(expected);
     })
   })
-  describe("when shuffled deck is missing cards", function () {
+  describe("when merged array is missing elements", function () {
     it("returns false", function () {
-      const actual = isSingleRiffle([1, 5], [2, 3, 6], [1,3,5,6]);
+      const actual = isMergedArray([1, 5], [2, 3, 6], [1,3,5,6]);
       const expected = false; 
       expect(actual).toEqual(expected);
     })
   })
-  describe("when shuffled deck has extra cards", function () {
+  describe("when merged array has extra elements", function () {
     it("returns false", function () {
-      const actual = isSingleRiffle([1, 5], [2, 3, 6], [1, 2, 3, 5, 6, 8]);
+      const actual = isMergedArray([4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6]);
       const expected = false;
       expect(actual).toEqual(expected);
     })
