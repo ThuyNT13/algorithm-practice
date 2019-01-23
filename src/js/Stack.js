84a72f69acbb295131fdd23867e08b32d46e6ccf
ThuyNT13 <ngo.thuy38@gmail.com>
2019-01-22 16:48:34 -0800

    Add tests and code for Stack implementation; pass all tests

---
+++ src/js/Stack.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(d) {
+    this.data = d;
+    this.next = null;
+  }
+}
+
+class StackLinkedList {
+  constructor() {
+    this.top = null;  // head
+    this.bottom = null; // tail
+    this.size = 0;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      throw new Error("Stack is empty!");
+    }
+
+    return this.top.data;
+  }
+
+  push(data) {
+    const newNode = new Node(data);
+
+    if (this.isEmpty()) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const temp = this.top;
+      this.top = newNode;
+      this.top.next = temp; 
+    }
+
+    this.size++;
+    return this;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      throw new Error("Stack is empty!");
+    }
+
+    const temp = this.top;
+
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    } 
+      
+    this.top = this.top.next; // sets top to null if next is null
+    this.size--;
+    return temp;
+  }
+
+  isEmpty() {
+    if (this.size <= 0) return true;
+  }
+}
---
+++ tests/spec/StackSpec.js
@@ -0,0 +1,50 @@
+describe("StackLinkedList", function() {
+  const Stack = new StackLinkedList();
+  
+  describe("push()", function() {
+    it("appends an item to top of empty stack", function() {
+      Stack.push("a");
+      const expected = "a";
+      expect(Stack.top.data).toEqual(expected);
+      expect(Stack.bottom.data).toEqual(expected);
+    })
+    it("appends another item to stack", function() {
+      Stack.push("b");
+      const expectedTop = "b";
+      const expectedBottom = "a";
+      expect(Stack.top.data).toEqual(expectedTop);
+      expect(Stack.bottom.data).toEqual(expectedBottom);
+    })
+  })
+  describe("peek()", function() {
+    it("returns item at the top of the stack", function() {
+      const expected = "b";
+      expect(Stack.peek()).toEqual(expected);
+    })
+  })
+  describe("pop()", function() {
+    it("removes an item from top of the stack", function() {
+      Stack.push("c");
+      Stack.pop();
+      const expected = "b";  
+      expect(Stack.top.data).toEqual(expected);
+    })
+    it("empties out stack and ensures bottom and top are null", function() {
+      Stack.pop();
+      Stack.pop();
+      const expected = null;
+      expect(Stack.top).toEqual(expected);
+      expect(Stack.bottom).toEqual(expected);
+    })
+    it("returns an error for attempting to pop an empty stack", function() {
+      expect( function() {
+        Stack.pop(); 
+      }).toThrow(new Error("Stack is empty!"))
+    })
+    it("returns an error for attempting to peek at empty stack", function() {
+      expect( function() {
+        Stack.peek();
+      }).toThrow(new Error("Stack is empty!"));
+    })
+  })
+})
