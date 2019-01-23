d6ba0b776a73cd469f17e3b46c034ec9fd46e6c0
ThuyNT13 <ngo.thuy38@gmail.com>
2019-01-23 14:12:20 -0800

    Rename variables

--- src/js/isMergedArray.js
+++ src/js/isMergedArray.js
@@ -1,18 +1,18 @@
 function isMergedArray(half1, half2, mergedArray) {
   let h1Index = half1.length - 1;
   let h2Index = half2.length - 1;
-  let sdIndex = mergedArray.length - 1;
+  let maIndex = mergedArray.length - 1;
 
-  while (sdIndex >= 0) {
+  while (maIndex >= 0) {
     const currentH1 = half1[h1Index];
     const currentH2 = half2[h2Index];
-    const currentSd = mergedArray[sdIndex];
+    const currentMa = mergedArray[maIndex];
 
-    if (currentH1 != undefined && currentH1 === currentSd) {
+    if (currentH1 != undefined && currentH1 === currentMa) {
       // half1.pop();
       // mergedArray.pop();
       h1Index--;
-    } else if (currentH2 != undefined && currentH2 === currentSd) {
+    } else if (currentH2 != undefined && currentH2 === currentMa) {
       // half2.pop();
       // mergedArray.pop();
       h2Index--;
@@ -20,11 +20,11 @@ function isMergedArray(half1, half2, mergedArray) {
       return false;
     }
 
-    sdIndex--;
+    maIndex--;
   }
 
   // checks for false positive when there are elements left in half stacks
-  if (h1Index < 0 && h2Index < 0 && sdIndex < 0) {
+  if (h1Index < 0 && h2Index < 0 && maIndex < 0) {
     return true;
   }
   return false;
